da5d744208b94bf1b9c25885183ed97895dc7d98
ds <benbryson789@users.noreply.github.com>
2021-08-27 19:15:06 -0400

    new changes

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -5,9 +5,20 @@ from .views import PostDetail
 app_name = 'blog_api'
 
 urlpatterns = [
-    #path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
-    #path('<int:pk>/', PostUpdate.as_view(), name='update'),
+#     #path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+#     #path('<int:pk>/', PostUpdate.as_view(), name='update'),
     path('<int:pk>/', Postcrud.as_view(), name='update'),
     path('', PostList.as_view(), name="listcreate"),
 
 ]
+
+
+# urlpatterns = [
+#     #path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+#     #path('<int:pk>/', PostUpdate.as_view(), name='update'),
+#     #commented out below aug 27 2021
+
+#     path('<int:pk>/', Postcrud.as_view(), name='update'),
+#     path('', PostList.as_view(), name="listcreate"),
+
+# ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -18,3 +18,23 @@ class PostUpdate(generics.UpdateAPIView):
 class Postcrud(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from blog.models import Post
+
+# from rest_framework.decorators import blog_api_view
+# from rest_framwork.response import Response
+
+# @blog_api_view(['GET'])
+# def apiOverview(request):
+    
+#     api_urls = {
+#         'List': '/post-list',
+#         'Detail View': '/post-detail/<str:pk>/',
+#         'Create': '/post-create/',
+#         'Update': '/post-update/<str:pk>/',
+#         'Delete':'/post-delete/<str:pk>/'
+#         }
+
+#     return Response(api_urls)
