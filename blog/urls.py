51a42f4da01d625d4a4796b5876090721feb5687
ds <benbryson789@users.noreply.github.com>
2021-08-26 22:53:52 -0400

    gitadd nav links and function views for blog app and blog url paths

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,9 @@ app_name = 'blog'
 urlpatterns = [
     # path('', TemplateView.asview(template_name="blog/index.html")),
     #path('', TemplateView.as_view(template_name="blog/index.html"),name="home"),
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('', views.postlist, name='home'),
+    path('about',views.about, name= 'about'),
+    path('policy',views.policy, name= 'policy'),
+    path('contact',views.contact, name= 'contact'),
+    path('<slug:slug>/', views.postDetail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,32 @@ class PostList(generic.ListView):
     queryset = Post.objects.all()
     template_name = 'blog/index.html'
 
+def postlist(request):
+    post = Post.objects.all()
+    context = {
+        "post_list":post
+
+    }
+    return render(request,'blog/index.html',context)
+
 class PostDetail(generic.DetailView):
+
     model = Post
     template_name = 'blog/post_detail.html'
+
+def postDetail(request,slug):
+    post = Post.objects.get(slug = slug)
+    print(post)
+    context = {
+        "post": post
+    }
+    return render(request,'blog/post_detail.html', context)
+
+def about(request):
+    return render(request,"blog/about.html",{})
+
+def policy(request):
+    return render(request,"blog/policy.html",{})
+
+def contact(request):
+    return render(request,"blog/contact.html",{})
